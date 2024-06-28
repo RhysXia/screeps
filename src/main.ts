9f628ec69c62cb826e3c1d80057df28710c7f6d5
RhysXia <xrs4433@outlook.com>
2024-06-28 19:09:01 +0800

    feat: update

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,39 @@
-import { Lifecycle } from "types/interfaces";
+import { ScreepsModule } from "types/interfaces";
 
-const modules: Array<Lifecycle> = [];
+// const modules: Array<ScreepsModule> = [];
 
-modules.forEach((it) => it.initialize?.());
+// modules.forEach((it) => it.initialize?.());
 
-export const loop = () => {
-  modules.forEach((it) => it.preProcess?.());
+// export const loop = () => {
+//   modules.forEach((it) => it.preProcess?.());
 
-  modules.forEach((it) => it.process?.());
+//   modules.forEach((it) => it.process?.());
 
-  modules.forEach((it) => it.postProcess?.());
+//   modules.forEach((it) => it.postProcess?.());
+// };
+
+const initializeModule = (modules: Array<ScreepsModule>) => {
+  const willInitedModules: Array<ScreepsModule> = [];
+  const initedModules: Array<ScreepsModule> = [];
+
+  modules.forEach((it) => {
+
+    if(initedModules.includes(it)) {
+      return
+    }
+
+    if(willInitedModules.includes(it)) {
+      throw new Error(`There may be circular dependencies between modules: `)
+    }
+
+    const deps = it.deps || [];
+    deps.forEach((it) => {
+      if (initedModules.includes(it)) {
+        return;
+      }
+
+      it.initialize?.();
+      initedModules.push(it);
+    });
+  });
 };
